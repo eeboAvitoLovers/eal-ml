8fdca0f4d558b7da3cd69c68cfbb20f1f4c68886
vlad-yar <118118809+vlad-yar@users.noreply.github.com>
2024-04-27 20:28:34 +0300

    Настроена запись в таблицу clusters

--- visualization.py
+++ visualization.py
@@ -1,7 +1,5 @@
 #!/usr/bin/env python
 # coding: utf-8
-from fastapi import FastAPI
-from pydantic import BaseModel
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
@@ -14,14 +12,18 @@ engine = create_engine("postgresql://eebo:eebo@194.87.234.96:5432/eebo")
 #inspector = inspect(engine)
 #print(inspector.get_table_names())
 
-table = 'messages'
-messages = pd.read_sql_table(table, con=engine, columns=['message'])
-preds = model.predict(messages['message'])
+tables = ['messages', 'clusters']
+messages = pd.read_sql_table(tables[0], con=engine, columns=['message'])
+clusters = model.predict(messages['message'])
 
-ready_data = pd.concat(objs=[messages, pd.DataFrame(preds, columns=['cluster'])], axis=1)
-ready_data[ready_data['cluster'] == preds[-1]]
+ready_data = pd.concat(objs=[messages, pd.DataFrame(clusters, columns=['cluster'])], axis=1)
 
 fig = plt.figure(figsize=(5, 4), dpi=150)
 sns.countplot(data=ready_data, x='cluster');
 plt.savefig('distrib_by_class.png')
 
+clusters = pd.DataFrame(clusters, columns=['clusters'])
+clusters.to_sql('clusters', con=engine, if_exists='replace', index=True, index_label='ticket_id')
+
+clust = pd.read_sql_table(tables[1], con=engine)
+print(clust)
