0eb35fae8af5460a5f61257de95c3dde759ff1a7
vlad-yar <118118809+vlad-yar@users.noreply.github.com>
2024-04-27 20:41:39 +0300

    Update work_with_tables.py

--- work_with_tables.py
+++ work_with_tables.py
@@ -4,16 +4,15 @@ import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pd
 import pickle
-with open('model.pkl', 'rb') as f:
-    model = pickle.load(f)
 from sqlalchemy import create_engine, inspect
 
 engine = create_engine("postgresql://eebo:eebo@194.87.234.96:5432/eebo")
-#inspector = inspect(engine)
-#print(inspector.get_table_names())
+
+with open('model.pkl', 'rb') as f:
+    model = pickle.load(f)
 
 tables = ['messages', 'clusters']
-messages = pd.read_sql_table(tables[0], con=engine, columns=['message'])
+messages = pd.read_sql_table(tables[0], con=engine, columns=['id', 'message'])
 clusters = model.predict(messages['message'])
 
 ready_data = pd.concat(objs=[messages, pd.DataFrame(clusters, columns=['cluster'])], axis=1)
@@ -23,7 +22,8 @@ sns.countplot(data=ready_data, x='cluster');
 plt.savefig('distrib_by_class.png')
 
 clusters = pd.DataFrame(clusters, columns=['clusters'])
-clusters.to_sql('clusters', con=engine, if_exists='replace', index=True, index_label='ticket_id')
+clusters['ticket_id'] = messages['id']
+clusters.to_sql('clusters', con=engine, if_exists='replace', index=False)
 
 clust = pd.read_sql_table(tables[1], con=engine)
 print(clust)
